226962c1377d0d585675add3c97b761fbead0bd5
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-13 00:22:00 +0200

    1. Athlete's mdel, called gpx_file, is created. 2. makemigrations and migrate is run with sqlite3 database. 3. Default settings.py is removed.

--- athlete/admin.py
+++ athlete/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import gpx_file
 # Register your models here.
+
+admin.site.register(gpx_file)
+
--- athlete/models.py
+++ athlete/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class gpx_file(models.Model):
+	docfile = models.FileField(upload_to='gpx/')
