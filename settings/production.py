53cc2b65392561ca77cb4e1444051160a2506d57
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-12 21:49:20 +0200

    1. Setting folder for local, stage and production is created.

---
+++ settings/production.py
@@ -0,0 +1,16 @@
+import os
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
