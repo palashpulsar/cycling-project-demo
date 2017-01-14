dd6e22c3c29345c2c039cfc4fab5ef08d0861cd5
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-14 18:06:00 +0200

    1. Another attempt to rectify the problem.

--- settings/base.py
+++ settings/base.py
@@ -15,8 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # LINK: https://github.com/heroku/python-getting-started/blob/master/gettingstarted/settings.py
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.join(BASE_DIR, 'pace_strategy')
 
+print "\n", PROJECT_ROOT, " -> PROJECT_ROOT"
+print BASE_DIR, " -> BASE_DIR \n"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
