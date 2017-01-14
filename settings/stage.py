fd7f1339651cd7cda2e6a9156c20717fb46e01a0
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-14 18:37:01 +0200

    1. Read the second answer on this link: http://stackoverflow.com/questions/31226097/not-getting-static-files-with-django-heroku-deployment The way static pages are deployed on heroku is alright for staging. However it requires additional work for production. So this point should be taken into account during further development of the project.

--- settings/stage.py
+++ settings/stage.py
@@ -24,17 +24,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# Django static
-# LINK: https://devcenter.heroku.com/articles/django-assets
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
