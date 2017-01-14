aad28d1547fa4c4b542975f62491d751d326fcbf
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-14 14:06:28 +0200

    1. Database for stage settings is configured for posygresql.

--- settings/stage.py
+++ settings/stage.py
@@ -9,9 +9,13 @@ DEBUG = True
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'pace_strategy',
+        'USER': 'palashsarkar',
+		'PASSWORD': 'FinLanD666',
+		'HOST': '',
+		'PORT': '',
     }
 }
 
