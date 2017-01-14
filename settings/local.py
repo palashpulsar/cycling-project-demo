8ffbcc9b5a50ace07c47236e1c7d65dcf147a0c4
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-14 13:58:11 +0200

    1. Change of database from sqlite3 to postgresql LINK: http://www.marinamele.com/taskbuster-django-tutorial/install-and-configure-posgresql-for-django

--- settings/local.py
+++ settings/local.py
@@ -10,8 +10,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pace_strategy',
+        'USER': 'palashsarkar',
+		'PASSWORD': 'FinLanD666',
+		'HOST': '',
+		'PORT': '',
     }
 }
 
