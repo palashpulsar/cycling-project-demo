bd209b041569215fbf04975a5fc30a8684cb6f8b
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-27 23:24:20 +0200

    Looking into the problem.

--- settings/stage.py
+++ settings/stage.py
@@ -30,3 +30,4 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'pace-stage'
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+print MEDIA_ROOT, " -> MEDIA_ROOT"
