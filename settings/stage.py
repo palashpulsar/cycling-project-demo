25792a426964cb5dbee562d0bd5474d55daa6942
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-27 23:20:24 +0200

    1. Playing with media file on Amazon S3.

--- settings/stage.py
+++ settings/stage.py
@@ -29,4 +29,4 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'pace-stage'
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
