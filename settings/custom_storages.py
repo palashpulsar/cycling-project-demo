9c8b86e255daca48d950f6eb48a259b32f979c2c
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-28 17:24:33 +0200

    Test

--- settings/custom_storages.py
+++ settings/custom_storages.py
@@ -1,10 +1,11 @@
 # LINK: https://www.caktusgroup.com/blog/2014/11/10/Using-Amazon-S3-to-store-your-Django-sites-static-and-media-files/
 
 from django.conf import settings
+from .stage import STATICFILES_LOCATION, MEDIAFILES_LOCATION
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
-	location = settings.STATICFILES_LOCATION
+	location = STATICFILES_LOCATION
 
 class MediaStorage(S3Boto3Storage):
-	location = settings.MEDIAFILES_LOCATION
+	location = MEDIAFILES_LOCATION
