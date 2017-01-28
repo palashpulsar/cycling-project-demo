aa025d9b64e2b546d943568aa927cfef73fe8e2a
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-28 16:41:23 +0200

    Test

--- settings/stage.py
+++ settings/stage.py
@@ -30,8 +30,8 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'pace-ire'
 # For media file
 # LINK: https://github.com/jamstooks/django-s3-folder-storage
-MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = '/media/'
+MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# MEDIA_ROOT = '/media/'
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # Fot static file
 STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
