328b35139ffd15695994d1e1064bd6da3a565dad
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-01-28 15:52:14 +0200

    Random corrections.

--- settings/stage.py
+++ settings/stage.py
@@ -19,15 +19,17 @@ DATABASES = {
     }
 }
 # Heroku Database (Link: https://devcenter.heroku.com/articles/django-app-configuration):
-# A demo for Derek
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# LINK: http://agiliq.com/blog/2014/06/heroku-django-s3-for-serving-media-files/
 INSTALLED_APPS += ('storages',)
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'pace-ire'
+# LINK: https://github.com/jamstooks/django-s3-folder-storage
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = '/media/'
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 print MEDIA_ROOT, " -> MEDIA_ROOT"
